ba37da8d9967edfa3267953dc13ecafdcfc347c8
sara latorre <saralatorre97@gmail.com>
2025-03-27 19:42:08 +0100

    deleted the ibj and bin folders from the repo

--- obj/Debug/net9.0/CSE_325_group_project.AssemblyInfo.cs
+++ obj/Debug/net9.0/CSE_325_group_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE_325_group_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+811a7be631a14ad0fd39b10cdd49b8133f64bd11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1564e52d7984efb91bb7355971a68809ed4450b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE_325_group_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE_325_group_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
